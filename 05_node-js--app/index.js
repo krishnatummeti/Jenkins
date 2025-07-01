a10315816ece7242fb5f5a97160c614fcaac5b1d
Krishna T <krishnatummeti@gmail.com>
2025-07-01 16:49:36 +0530

    Added index.js and package.json for Jenkins Node.js pipeline

--- 05_node-js--app/index.js
+++ 05_node-js--app/index.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
   res.send('Hello from Jenkins Node.js App!');
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
