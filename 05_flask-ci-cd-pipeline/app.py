096bd92638d790e584e135cce667a749e32324ae
Krishna T <krishnatummeti@gmail.com>
2025-07-13 16:08:49 +0530

    adding 05_flask-ci-cd-pipeline

--- 05_flask-ci-cd-pipeline/app.py
+++ 05_flask-ci-cd-pipeline/app.py
@@ -1,49 +1,10 @@
-pipeline {
-    agent { label 'AGENT-01' }
+from flask import Flask
 
-    environment {
-        APP_SERVER = 'devuser@192.168.71.129'
-        APP_DIR = '/home/devuser/flask-app'
-        CODE_DIR = '05_flask-ci-cd-pipeline'
-    }
+app = Flask(__name__)
 
-    stages {
-        stage('Clone Repo') {
-            steps {
-                git branch: 'main', url: 'https://github.com/krishnatummeti/Jenkins.git'
-            }
-        }
+@app.route('/')
+def home():
+    return 'Hello from Jenkins CI/CD Pipeline!'
 
-        stage('Install & Test') {
-            steps {
-                dir(CODE_DIR) {
-                    sh '''
-                    pip3 install -r requirements.txt --user
-                    python3 test_app.py
-                    '''
-                }
-            }
-        }
-
-        stage('Deploy') {
-            steps {
-                dir(CODE_DIR) {
-                    sh '''
-                    ssh $APP_SERVER 'rm -rf $APP_DIR && mkdir -p $APP_DIR'
-                    scp app.py requirements.txt $APP_SERVER:$APP_DIR
-                    ssh $APP_SERVER 'cd $APP_DIR && pip3 install -r requirements.txt --user && nohup python3 app.py > app.log 2>&1 &'
-                    '''
-                }
-            }
-        }
-    }
-
-    post {
-        success {
-            echo '✅ Deployment Complete!'
-        }
-        failure {
-            echo '❌ Something went wrong.'
-        }
-    }
-}
+if __name__ == '__main__':
+    app.run(debug=True)
