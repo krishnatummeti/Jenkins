9ba2145b60b8b49efd6cb0a97029ad3d79820b63
Krishna T <krishnatummeti@gmail.com>
2025-08-03 11:20:26 +0530

    Adding 06_simple-java-maven-app project

--- 06_simple-java-maven-app/jenkins/scripts/deliver.sh
+++ 06_simple-java-maven-app/jenkins/scripts/deliver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "=== Deliver Stage Started ==="
 java -cp target/simple-java-maven-app-1.0-SNAPSHOT.jar com.example.App
-echo "=== Deliver Stage Completed ==="
+echo "==== Deliver Stage Completed ===="
 
 
