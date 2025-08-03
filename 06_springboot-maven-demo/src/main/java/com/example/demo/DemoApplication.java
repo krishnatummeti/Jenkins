e1ee6013e3459e7693589d7ccad65c69ba031998
Krishna T <krishnatummeti@gmail.com>
2025-08-03 15:02:23 +0530

    change the file location

--- 06_springboot-maven-demo/src/main/java/com/example/demo/DemoApplication.java
+++ 06_springboot-maven-demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,15 +1,11 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-@SpringBootTest
-class DemoApplicationTests {
-
-    @Test
-    void contextLoads() {
-        assertThat(true).isTrue();
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
     }
 }
